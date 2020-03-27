3cf0db55bc5232ed38c61cbd662af21cc9da3f45
dusanpopov <aristocut90@gmail.com>
2020-03-27 19:09:04 +0100

    Updates

---
+++ precache-manifest.f8bcf42690e3302f71a341b5e82b28e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff9731b90c2b69b996c438214103322",
+    "url": "/Food-recipe-search/index.html"
+  },
+  {
+    "revision": "9edf97c4c8a724282542",
+    "url": "/Food-recipe-search/static/css/main.0b89506e.chunk.css"
+  },
+  {
+    "revision": "5bb4df411ef83e3866ab",
+    "url": "/Food-recipe-search/static/js/2.4d9d18cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Food-recipe-search/static/js/2.4d9d18cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9edf97c4c8a724282542",
+    "url": "/Food-recipe-search/static/js/main.65d6025a.chunk.js"
+  },
+  {
+    "revision": "7f97fd968698726cad83",
+    "url": "/Food-recipe-search/static/js/runtime-main.bfafa4a0.js"
+  }
+]);
